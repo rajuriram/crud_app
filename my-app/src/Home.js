d3a2a67e29f6b1f756a85ad60a3a2a271fe9c433
Ram <rajuriram@gmail.com>
2023-01-03 23:57:19 +0530

    Home_page_modified

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -10,6 +10,15 @@ export default function Home(){
                 <h2>
                     This is a sample username and password creator and modifier
                 </h2>
+                <h3>
+                    This sample app consists of Two major components Create Data and Show Data
+                </h3>
+                <h4>
+                    <b>Create Data</b> is used to create data by entring the username and password.
+                </h4>
+                <h4>
+                    <b>Show data</b> is used to view the data that has been created and there is an option to modify password and delete user where the data can be modified or it can be deleted.
+                </h4>
             </div>
 
 
